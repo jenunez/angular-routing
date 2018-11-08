dcc607d257fa122b601bc6f64f9a33611029dfa3
Jesus Nunez <jesus.nunez@incontact.com>
2018-11-08 17:08:35 -0400

    Unit Test and e2e

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users: any[];
+  hasPermission: false;
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermission) {
+      this.getUsers()
+      .then(users => this.users = users)
+      .catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
+  }
+
+  async getUsers() {
+    return [
+      {name: 'Jesus', email: 'jesus@angular.com'},
+      {name: 'John', email: 'john@angular.com'}
+    ];
   }
 
 }
